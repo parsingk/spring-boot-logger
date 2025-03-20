9f118e32cecf5743f618e9e2ab12736d4233010d
parkjp <parkjp@anipen.com>
2025-03-21 08:08:34 +0900

    update

--- src/main/java/com/spring/boot/logger/application/IApplicationLoggerAspect.java
+++ src/main/java/com/spring/boot/logger/application/IApplicationLoggerAspect.java
@@ -5,9 +5,36 @@ import org.aspectj.lang.ProceedingJoinPoint;
 
 public interface IApplicationLoggerAspect {
 
+    /**
+     * AOP Around - Method with @Controller Annotation.
+     * Except Method with @NoApplicationLog and @Error Annotation.
+     *
+     * @param joinPoint
+     * @param requestContext
+     * @return
+     * @throws Throwable
+     */
     Object doAround(ProceedingJoinPoint joinPoint, RequestContext requestContext) throws Throwable;
 
+    /**
+     * AOP Around - Method with @Controller and @Error Annotation.
+     * Except Method with @NoApplicationLog Annotation.
+     *
+     * @param joinPoint
+     * @param requestContext
+     * @return
+     * @throws Throwable
+     */
     Object errorAround(ProceedingJoinPoint joinPoint, RequestContext requestContext) throws Throwable;
 
+    /**
+     * AOP Around - Method with @ExceptionHandler Annotation.
+     * Except ApiException class.
+     *
+     * @param joinPoint
+     * @param requestContext
+     * @return
+     * @throws Throwable
+     */
     Object doExceptionHandlerAround(ProceedingJoinPoint joinPoint, RequestContext requestContext) throws Throwable;
 }
